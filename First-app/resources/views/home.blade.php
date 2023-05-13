9b76c03780c4661ce732ad09a6080513a835d96d
Hasala <hasaladissanayake@yahoo.com>
2023-05-13 09:46:24 +0530

    post model, create post, routes related functionalities

--- First-app/resources/views/home.blade.php
+++ First-app/resources/views/home.blade.php
@@ -14,6 +14,16 @@
             @csrf
             <button>Log Out</button>
         </form>
+
+        <div style="border: 1px solid black; padding: 2rem">
+            <h2>Create a Post</h2>
+            <form action="/create-post" method="post" style="display: flex; flex-direction: column; gap: 2rem">
+                @csrf
+                <input type="text" name="title" placeholder="post-title">
+                <textarea name="body" cols="30" rows="5" placeholder="post-body"></textarea>
+                <button>Save Post</button>
+            </form>
+        </div>
     @else
     <div style="border: 1px solid black; padding: 2rem">
         <h2>Register</h2>
