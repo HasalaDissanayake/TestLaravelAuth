3ae161e215eed86a216df62bede6a526b215f5c8
Hasala <hasaladissanayake@yahoo.com>
2023-05-13 10:42:36 +0530

    added editing functionality

--- First-app/app/Http/Controllers/PostController.php
+++ First-app/app/Http/Controllers/PostController.php
@@ -21,6 +21,26 @@ class PostController extends Controller
     }
 
     public  function  showEditView(Post $post){
+        if(auth()->user()->id !== $post['user_id']){
+            return redirect('/');
+        }
         return view('edit-post',['post'=>$post]);
     }
+
+    public function updatedPost(Post $post, Request $request){
+        if(auth()->user()->id !== $post['user_id']){
+            return redirect('/');
+        }
+        $incomingData = $request->validate([
+           'title' => 'required',
+           'body' => 'required'
+        ]);
+
+        $incomingData['title'] = strip_tags($incomingData['title']);
+        $incomingData['body'] = strip_tags($incomingData['body']);
+
+        $post->update($incomingData);
+        return redirect('/');
+    }
+
 }
--- First-app/resources/views/edit-post.blade.php
+++ First-app/resources/views/edit-post.blade.php
@@ -4,14 +4,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Edit Post</title>
 </head>
 <body>
     <h2>Edit Post</h2>
     <form action="/edit-post/{{$post->id}}" method="post">
         @csrf
         @method('PUT')
-        <input type=" text" name-="title" value="{{$post->title}}">
+        <input type="text" name="title" value="{{$post->title}}">
         <textarea name="body" cols="30" rows="5" placeholder="post-body">{{$post->body}}</textarea>
         <button>Save Changes</button>
     </form>
